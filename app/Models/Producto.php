adc86ca73a715cd9de2018abf1a32568134d5482
OppaDev <jlobando2@espe.edu.ec>
2025-07-22 00:08:20 -0500

    fixRelation 'categoria' is not found in App\Models\Producto model.

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Categoria;
+use App\Models\FacturaDetalle;
+use App\Models\User;
 
 class Producto extends Model
 {
